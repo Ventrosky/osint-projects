35060d998186f9099f1ec19a5009ec44c49814a7
Salvatore Ventrone <ventrosky@gmail.com>
2018-07-09 08:08:54 +0200

    Update scriptsChrome.js

--- facebook-activity/scriptsChrome.js
+++ facebook-activity/scriptsChrome.js
@@ -1,24 +1,8 @@
 
-
-
 var scroll = setInterval(function(){ window.scrollBy(0,1000); }, 2000);
 
 clearInterval(scroll);
-/*
-<script>
-function start_scroll_down() { 
-   scroll = setInterval(function(){ window.scrollBy(0, 1000); console.log('start');}, 1500);
-}
-function stop_scroll_down() {
-   clearInterval(scroll);
-   console.log('stop');
-}
-</script>
-<button onclick="start_scroll_down();">Start Scroll</button>
-<button onclick="stop_scroll_down();">Stop Scroll</button>*/
 
-
-//_5pbx userContent _3576
 abbr_tags = document.getElementsByTagName("abbr")
 
 timestamps = ""
