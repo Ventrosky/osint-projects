599b72a4764adf876d900509d6f678ed0ebd3d76
Salvatore Ventrone <ventrosky@gmail.com>
2018-07-22 15:13:50 +0200

    Update setup.sh

--- dark-web/setup.sh
+++ dark-web/setup.sh
@@ -6,7 +6,7 @@ pip install stem
 bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
 [[ -s "$HOME/.gvm/scripts/gvm" ]] && source "$HOME/.gvm/scripts/gvm"
 source /root/.gvm/scripts/gvm
-gvm install go1.10
+gvm install go1.10 --binary
 gvm use go1.10
 go get github.com/s-rah/onionscan
 go install github.com/s-rah/onionscan
